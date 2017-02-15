ea54f2ceefa0f8cedd460181b4d1811fecf6cb64
doctor3397 <hs2546@columbia.edu>
2017-02-15 14:50:28 -0500

    iQuery toggelClass for click event

--- js/main.js
+++ js/main.js
@@ -1,2 +1,16 @@
+$(function() {
+  // When someone hits the button in the switch, remove the current state class (on/off), and apply the opposite class (on/off)
+  $('.switch').click(function(){
+    // $('button').removeClass('on').addClass('off');
+    $('button').toggleClass('on off');
+    $('body').toggleClass('light dark');
 
+    if( $('h1.status').text() === "It's so bright in here!" ){
+      $('h1.status').text("Hey, who turned off the lights?");
 
+    } else{
+      $('h1.status').text("It's so bright in here!");
+    }
+  });
+
+});
